4968f185bc09f6053f2b746f1b6608da1eb7056e
jonnyds <schwarz.jonathan@icloud.com>
2017-12-05 18:01:54 +0200

    update

--- main.cpp
+++ main.cpp
@@ -4,10 +4,19 @@
 using namespace std;
 int main() {
     Server server(8000);
+  //  Server server1(8001);
     try {
         server.start();
+    //    server1.start();
     } catch (const char *msg) {
         cout << "Cannot start server. Reason: " << msg << endl;
+        exit(-1);
+    }
+    Server server1(8000);
+    try {
+        server1.start();
+    } catch (const char *msg) {
+        cout << "Cannot start server1. Reason: " << msg << endl;
         exit(-1);
     }
 }
