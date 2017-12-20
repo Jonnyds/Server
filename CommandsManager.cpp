bbc7e1b9dce55502b2e5cd76746ecc91889bbf5e
NoamRI <noam.ri02gmail.com>
2017-12-20 16:39:50 +0200

    server: command commandsMananger

--- CommandsManager.cpp
+++ CommandsManager.cpp
@@ -8,10 +8,14 @@
 
 
 
+void CommandsManager:: executeCommand (string command, vector<string> args) {
 
+    Command *commandObj = commandsMap[command];
+    commandObj ->execute(args);
+}
 
 CommandsManager:: ~CommandsManager() {
-    
+
     map<string, Command *>::iterator it;
     for (it = commandsMap.begin(); it != commandsMap.end(); it++) {
         delete it ->second;
