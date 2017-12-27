89af6bfc09b74900e4fa2912151de41bbf71cb72
jonnyds <schwarz.jonathan@icloud.com>
2017-12-27 19:25:19 +0200

    update

---
+++ GameManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jonathan on 27/12/17.
+//
+
+#include "GameManager.h"
---
+++ GameManager.h
@@ -0,0 +1,14 @@
+//
+// Created by jonathan on 27/12/17.
+//
+
+#ifndef SERVER_GAMEMANAGER_H
+#define SERVER_GAMEMANAGER_H
+
+
+class GameManager {
+
+};
+
+
+#endif //SERVER_GAMEMANAGER_H
