b6b646b7509965f03b199075f513c45d0f626de6
jonnyds <schwarz.jonathan@icloud.com>
2018-01-23 18:11:25 +0200

    update

--- ThreadPool.cpp
+++ ThreadPool.cpp
@@ -5,12 +5,10 @@
 #include "ThreadPool.h"
 #include <unistd.h>
 
-ThreadPool::ThreadPool(int threadsNum) :
-        stopped(false) {
+ThreadPool::ThreadPool(int threadsNum) : stopped(false) {
     threads = new pthread_t[threadsNum];
     for (int i = 0; i < threadsNum; i++) {
-        pthread_create(threads + i, NULL, execute,
-                       this);
+        pthread_create(threads + i, NULL, execute, this);
     }
     pthread_mutex_init(&lock, NULL);
 }
